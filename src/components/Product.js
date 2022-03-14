54d27a8bdb1af76ea1dfdf3fae39e4e750aba21e
mihai133 <mihai.cngm@gmail.com>
2022-03-14 12:12:32 +0200

    Visual update for the component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,33 +1,65 @@
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
-
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native'
+import {Feather} from '@expo/vector-icons'
 
 
 const ProductComponent = (props) => {
+  console.log(props)
   const navigation = useNavigation()
   return (
     <TouchableOpacity 
     onPress={() => navigation.navigate('Product Screen')}
-    style={Style.touchable}
+    style={Styles.touchable}
     
     >
-        <Text>Product Component</Text>
+      
+        <Image style={Styles.image} source={props.imageSource} />
+        <View style={Styles.productDescribtion}>
+        
+        {/* title */}
+          <Text style={Styles.productTitle}>{props.title}</Text>
+        
+        {/* rating */}
+          <View style={{flexDirection:'row', paddingRight:20,}}>
+            <Feather name='star' size={20} />
+            <Text style={Styles.productTitle}>{props.rating}</Text>
+          </View>
+        </View>
     </TouchableOpacity>
   )
 }
 
-const Style = StyleSheet.create({
+const Styles = StyleSheet.create({
     touchable: {
-
-        
-        height: "20%",
-        width: "50%",
-        alignItems: 'center',
-        justifyContent: 'center',
+      marginTop:50,
+        height: 200,
+        width: "100%",
         backgroundColor: "lightgrey",
-        borderRadius:20,
+        borderRadius:10,
+        
     },
+
+    image:{
+      borderTopLeftRadius:10,
+      borderTopRightRadius:10,
+      height: "70%",
+      width:'100%',
+      top:-20,
+      marginBottom: -5,
+      position:'relative',
+    },
+
+    productDescribtion:{
+      paddingLeft:10,
+      flexDirection:'row',
+      justifyContent:'space-between'
+    },
+    productTitle: {
+      fontSize:18,
+      fontWeight:'700',
+    },
+
 })
 
 export default ProductComponent
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 
 import React from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity, Button } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button, TextInput, StatusBar } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {Feather} from '@expo/vector-icons'
 import ProductComponent from '../components/Product'
 
-const HomeScreen = (props) => {
-    console.log(props)
+const HomeScreen = ( {navigation}) => {
   return (
     <SafeAreaView style={styles.screen}>
+        <StatusBar backgroundColor="#61dafb"/>
         <View style={styles.headerLogoMenu}>   
             {/* Logo */}
             <TouchableOpacity>
@@ -20,9 +20,24 @@ const HomeScreen = (props) => {
         <Text style={styles.h1}>Find Good food around you</Text>
 
         {/* Search Bar */}
-
+        <View style={{marginTop:10 , width:"100%", height: 40, borderRadius:10, alignItems:'center', backgroundColor:'lightgrey', flexDirection:'row'}}>
+            <Feather name='search' size={30} style={{paddingRight:15, paddingLeft:10,}} />
+            <TextInput placeholder='Search for food' style={{height:40, width:"80%"}}/>
+        </View>
         {/* Product Component */}
-       <ProductComponent navigation={()=> props.navigation.navigate('Product Screen')} />
+        {/* Here is going to be a FlatList of Restaurants */}
+       <ProductComponent
+            title={<Text>Salad Restaurant</Text>}
+            rating={<Text>4.5 / 5</Text>}
+            imageSource={{uri:'https://images.unsplash.com/photo-1602881917760-7379db593981?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2360&q=80'}} 
+            navigation={()=> navigation.navigate('Product Screen')} 
+       />
+       <ProductComponent
+            title={<Text>Sushi Restaurant</Text>}
+            rating={<Text>4.8 / 5</Text>}
+            imageSource={{uri:'https://images.unsplash.com/photo-1635526910429-051cf1ed127e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80'}} 
+            navigation={()=> navigation.navigate('Product Screen')} 
+       />
     </SafeAreaView>
   )
 }
