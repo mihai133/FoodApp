0cb465a1f6022daa050741c0ed87ea8eaa14c252
Mihai Bancila <68776490+mihai133@users.noreply.github.com>
2022-03-20 23:42:46 +0200

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const Api =  axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers:{
-        Authorization: 'Bearer vxUYi4WtaX2R5lCDgzA49QRNqFvgWr6Mikp7fUOeXMKGPegiegxPLhioZSt1DTorhOgvH4EzedduOGHUOS7kvZzVL9xwpmyKvZy1CIUIAt3F9nb29qlfqXG8bsf7YXYx'
+        Authorization: 'Bearer ******
     }
 })
 
